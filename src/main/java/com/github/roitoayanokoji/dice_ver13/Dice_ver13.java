0dba78a258eb7175fa281b9bd78cce5b11eeba91
senganull <ayanokoji.roito@gmail.com>
2024-12-27 12:21:17 +0900

    レポジトリ作成

--- src/main/java/com/github/roitoayanokoji/dice_ver13/Dice_ver13.java
+++ src/main/java/com/github/roitoayanokoji/dice_ver13/Dice_ver13.java
@@ -7,11 +7,15 @@ public final class Dice_ver13 extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("OnEnable");
+
+        getCommand("dice").setExecutor(new DiceCommandExecutor(this));
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("OnDisable");
     }
 }
