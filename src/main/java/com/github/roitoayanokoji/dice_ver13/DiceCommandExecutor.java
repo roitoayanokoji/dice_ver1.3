8771ee9d50c3b055d2c49b8bf285c9328ac7ebb2
senganull <ayanokoji.roito@gmail.com>
2024-12-27 12:13:59 +0900

    Initial commit

---
+++ src/main/java/com/github/roitoayanokoji/dice_ver13/DiceCommandExecutor.java
@@ -0,0 +1,4 @@
+package com.github.roitoayanokoji.dice_ver13;
+
+public class DiceCommandExecutor {
+}
---
+++ src/main/java/com/github/roitoayanokoji/dice_ver13/Dice_ver13.java
@@ -0,0 +1,17 @@
+package com.github.roitoayanokoji.dice_ver13;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dice_ver13 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
